e07e27098ad6bcd24703b5b749aa5f4f585bc916
RihabZA <rihab1994.zamouri@gmail.com>
2022-05-08 00:44:09 +0100

    Picture Info

--- src/pages/imageDetails/index.js
+++ src/pages/imageDetails/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { MainContainer } from "../../components";
 import { getImageDetails } from "../../backend";
 import { useParams } from "react-router-dom";
+import styles from "./styles.module.css";
 
 function ImageDetails() {
   const [details, setDetails] = useState({});
@@ -18,7 +19,26 @@ function ImageDetails() {
       );
   }, [id]);
 
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <div className={styles.PictureDetails}>
+        <div className={styles.Col2}>
+          <img
+            src={details.largeImageURL}
+            className={styles.Picture}
+            alt={details.tags}
+          />
+        </div>
+        <div className={styles.PictureInfo}>
+          <p>Name of photographer: {details.user}</p>
+          <p>Tags: {details.tags}</p>
+          <p>Views: {details.views}</p>
+          <p>Downloads: {details.downloads}</p>
+          <p>Size: {details.imageSize}</p>
+        </div>
+      </div>
+    </MainContainer>
+  );
 }
 
 export default ImageDetails;
